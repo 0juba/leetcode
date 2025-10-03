13746c7fb47f3db3b057e9bef964a9a1e1b4d90f
Andrei Guba <andrei.guba@tabby.ai>
2025-10-03 22:33:43 +0100

    passed

--- internal/pkg/tasks/substringwithconcatenationofallwords/main.go
+++ internal/pkg/tasks/substringwithconcatenationofallwords/main.go
@@ -1,6 +1,8 @@
 package substringwithconcatenationofallwords
 
-import "maps"
+import (
+	"sort"
+)
 
 func findSubstring(s string, words []string) []int {
 	if len(words) == 0 {
@@ -20,10 +22,10 @@ func findSubstring(s string, words []string) []int {
 	wordLen := len(words[0])
 	substrLen := wordLen
 
-	total := 0
+	total := len(words)
+
 	hash := make(map[string]int, len(words))
 	for _, w := range words {
-		total++
 		if _, ok := hash[w]; ok {
 			hash[w] += 1
 		} else {
@@ -31,22 +33,39 @@ func findSubstring(s string, words []string) []int {
 		}
 	}
 
-	calc := make(map[string]int, len(words))
+	wordsIndex := make([]string, 0, len(words))
+	for w := range hash {
+		wordsIndex = append(wordsIndex, w)
+	}
+
+	sort.Strings(wordsIndex)
+
+	stat := make([]int, len(wordsIndex))
+	for i, w := range wordsIndex {
+		stat[i] = hash[w]
+	}
+
+	calc := make([]int, len(wordsIndex))
 	for i := 0; i < len(s)-substrLen+1; i++ {
-		maps.Copy(calc, hash)
+		copy(calc, stat)
 		foundMatches := 0
 		for j := i; j < len(s)-substrLen+1; {
 			part := s[j : j+substrLen]
-			if _, ok := hash[part]; !ok {
+			idx := sort.SearchStrings(wordsIndex, part)
+			if idx >= len(wordsIndex) {
 				break
 			}
 
-			if calc[part] == 0 {
+			if wordsIndex[idx] != part {
+				break
+			}
+
+			if calc[idx] == 0 {
 				break
 			}
 
 			foundMatches++
-			calc[part]--
+			calc[idx]--
 			j += wordLen
 			if foundMatches == total {
 				break
